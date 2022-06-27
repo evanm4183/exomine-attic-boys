8494ab414c9787fb6921670561addd9f641a694e
Evan Mitchell <evanm4183@gmail.com>
2022-06-27 14:19:03 -0500

    governors module completed

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -1,10 +1,28 @@
-import { getGovernors } from "./database.js";
+import { getGovernors, setColony } from "./database.js";
+
+document.addEventListener("change", event => {
+    if (event.target.id === "governors-dropdown") {
+        const governorId = parseInt(event.target.value)
+        setColony(governors.find(gov => gov.id === governorId).colonyId)
+
+        const colonyResourcesElement = document.querySelector(".colMin")
+        const facilitiesDropdownElement = document.querySelector(".facChoice")
+
+        colonyResourcesElement.innerHTML = ColonyResourcesHTML() // function name subject to change
+        facilitiesDropdownElement.innerHTML = FacilitiesHTML() // function name subject to change
+    }
+})
 
 const governors = getGovernors();
 
 export const GovernorsHTML = () => {
-    let html = `<select class="governors-dropdown"><option value="0">Select a Governor...</option>`;
+    let html = `<select id="governors-dropdown"><option value="0">Select a Governor...</option>`;
     const activeGovernors = governors.filter(governor => governor.isActive);
 
+    for (const governor of activeGovernors) {
+        html += `<option value="${governor.id}">${governor.name}</option>`;
+    }
+    html += `</select>`;
 
+    return html;
 }
