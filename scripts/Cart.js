8dfd6a4df175d84dea4b56a4dbf3cda3816f411d
rjdny <therealglatrix@gmail.com>
2022-06-28 09:25:13 -0500

    fixed cart imports and added stuff to render

--- scripts/Cart.js
+++ scripts/Cart.js
@@ -1,15 +1,20 @@
-import { getCartMineral } from "./database"
-import { getMinerals } from "./database"
-import { executePurchase } from "./database";
+import { getCartMineral } from "./database.js"
+import { getMinerals } from "./database.js"
+import { executePurchase } from "./database.js";
 
-document.getElementById("buyButton").addEventListener("click", () => {
-    executePurchase();
+document.addEventListener("click", (event) => {
+    if (event.target.id == "buyButton") {
+        executePurchase();
+    }
 });
+
 
 export const CartHTML = () => {
     if (getCartMineral().id != -1) {
         const currMin = getMinerals().find(x => x.id == getCartMineral().id)
-        return `<p class="cartmin">${currMin.name}</p>`;
+        if (currMin != undefined) {
+            return `<p class="cartmin">${currMin.name ?? ""}</p>`;
+        }
     }
     return "";
 }
