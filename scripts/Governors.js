a2a38079d011ec2412a03f2d2391df17d49849d1
Ryan Downey <105568842+rjdny@users.noreply.github.com>
2022-06-28 14:13:24 -0500

    livecode-bug-fixes

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -2,30 +2,39 @@ import { getGovernors, setColony } from "./database.js";
 import { ColonyResourcesHTML } from "./ColonyResources.js";
 import { FacilitiesHTML } from "./Facilities.js";
 
-document.addEventListener("change", event => {
-    if (event.target.id === "governors-dropdown" && event.target.value != 0) {
-        const governorId = parseInt(event.target.value);
-        setColony(governors.find(gov => gov.id === governorId).colonyId);
+document.addEventListener("change", (event) => {
+  if (event.target.id === "governors-dropdown" && event.target.value != 0) {
+    const governorId = parseInt(event.target.value);
+    setColony(governors.find((gov) => gov.id === governorId).colonyId);
 
-        const colonyResourcesElement = document.querySelector(".colMin");
-        const facilitiesDropdownElement = document.querySelector(".facChoice");
+    const colonyResourcesElement = document.querySelector(".colMin");
+    const facilitiesDropdownElement = document.querySelector(".facChoice");
 
-        colonyResourcesElement.innerHTML = ColonyResourcesHTML();
-        facilitiesDropdownElement.innerHTML = FacilitiesHTML();
-        document.querySelector(".cartFac").innerHTML = ""
-    }
-})
+    colonyResourcesElement.innerHTML = ColonyResourcesHTML();
+    facilitiesDropdownElement.innerHTML = FacilitiesHTML();
+    document.querySelector(".cartFac").innerHTML = "";
+  } else if (
+    event.target.id === "governors-dropdown" &&
+    event.target.value == 0
+  ) {
+    document.querySelector(
+      ".facChoice"
+    ).innerHTML = `<select name="facility"><option value="0">Choose a facility</option></select>`;
+    document.querySelector(".cartFac").innerHTML = "";
+    document.querySelector(".colMin").innerHTML = "<h5>Colony Minerals</h5>";
+  }
+});
 
 const governors = getGovernors();
 
 export const GovernorsHTML = () => {
-    let html = `<select id="governors-dropdown"><option value="0">Select a Governor...</option>`;
-    const activeGovernors = governors.filter(governor => governor.isActive);
+  let html = `<select id="governors-dropdown"><option value="0">Select a Governor...</option>`;
+  const activeGovernors = governors.filter((governor) => governor.isActive);
 
-    for (const governor of activeGovernors) {
-        html += `<option value="${governor.id}">${governor.name}</option>`;
-    }
-    html += `</select>`;
+  for (const governor of activeGovernors) {
+    html += `<option value="${governor.id}">${governor.name}</option>`;
+  }
+  html += `</select>`;
 
-    return html;
-}
+  return html;
+};
--- scripts/Minerals.js
+++ scripts/Minerals.js
@@ -1,32 +1,36 @@
-import { CartHTML } from "./Cart.js"
-import { getMineralsInFacilities, getMinerals, setMineral } from "./database.js"
+import { CartHTML } from "./Cart.js";
+import {
+  getMineralsInFacilities,
+  getMinerals,
+  setMineral
+} from "./database.js";
 
 // Retrieve the relevant tables
 
-
-
 // Function that builds the radio buttons for the minerals in facilities with a given facility id
 // Returns an html string of the radio buttons for Facilities.js
-export const MineralsHTML = facilityId => {
-    const allMinInFac = getMineralsInFacilities()
-    const minerals = getMinerals()
-    let html = ""
+export const MineralsHTML = (facilityId) => {
+  const allMinInFac = getMineralsInFacilities();
+  const minerals = getMinerals();
+  let html = "<ul>";
 
-    // Iterate through the minerals in facilities table
-    for (const minInFac of allMinInFac) {
-        if (minInFac.facilityId === facilityId) {
-            let mineralName = minerals.find(x => x.id === minInFac.mineralId).name 
-            html += `
-                <input type="radio" name="mineralOption" value="${minInFac.mineralId}">${minInFac.amountOwned} tons of ${mineralName}`
-        }
+  // Iterate through the minerals in facilities table
+  for (const minInFac of allMinInFac) {
+    if (minInFac.facilityId === facilityId) {
+      let mineralName = minerals.find((x) => x.id === minInFac.mineralId).name;
+      if (minInFac.amountOwned > 0) {
+        html += `
+                <li><input type="radio" name="mineralOption" value="${minInFac.mineralId}">${minInFac.amountOwned} tons of ${mineralName}</li>`;
+      }
     }
+  }
 
-    html += ""
-    return html
-}
+  html += "</ul>";
+  return html;
+};
 document.addEventListener("change", (event) => {
-    if (event.target.name === "mineralOption") {
-        setMineral(parseInt(event.target.value))
-        document.querySelector('.itemsInCart').innerHTML = CartHTML()
-    }
-})
+  if (event.target.name === "mineralOption") {
+    setMineral(parseInt(event.target.value));
+    document.querySelector(".itemsInCart").innerHTML = CartHTML();
+  }
+});
